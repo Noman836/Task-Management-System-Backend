033d09f6d5cf28c58ab14cb94aa1ebb6569c84a0
Noman836 <rao977684@gmail.com>
2026-02-13 09:56:14 +0500

    fixes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:3000','https://task-management-system-frontend-1w4v-bht5omyxa.vercel.app/','https://*.vercel.app'],
+  origin: ['http://localhost:3000','https://task-management-system-frontend-1w4v-bht5omyxa.vercel.app/'],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
